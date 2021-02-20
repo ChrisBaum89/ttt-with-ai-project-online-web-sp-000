88c795d3854ff137ba5f61e273e4f991e22ca53f
Chris Baum <christopher.baum@valpo.edu>
2021-02-20 20:26:28 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -57,6 +57,7 @@ class Game
     if board.valid_move?(input)
       board.update(input, current_player)
     else
+      board.display
       puts "Invalid move. Please select another position."
       turn
     end
