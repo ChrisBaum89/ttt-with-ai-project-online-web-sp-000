9f83c91240de3128f310a1b83c4b7d133fdaf3bd
Chris Baum <christopher.baum@valpo.edu>
2021-02-20 20:23:31 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,7 @@ module Players
     WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
    def move(board)
-      #binding.pry
+      binding.pry
       num_of_empty = board.cells.count(" ")
       #random number to start
       if num_of_empty > 7
