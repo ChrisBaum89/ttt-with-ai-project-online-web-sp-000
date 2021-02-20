a69c263941ff629b842291d47de498725e006234
Chris Baum <christopher.baum@valpo.edu>
2021-02-20 21:09:00 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,7 @@
 module Players
   class Computer < Player
     WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
-    
+
     def move(board)
       WIN_COMBINATIONS.each do |x|
         pos_1_val = board.cells[x[0]]
