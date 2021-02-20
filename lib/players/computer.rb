c18962486d2a21581e2d14c3d7da44d93ec56d24
Chris Baum <christopher.baum@valpo.edu>
2021-02-20 21:04:18 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,11 +9,11 @@ module Players
         pos_2_val = board.cells[x[1]]
         pos_3_val = board.cells[x[2]]
         if pos_1_val == pos_2_val && (pos_1_val == "X" || pos_1_val == "O")
-          x[2]
+          "#{x[2]}"
         elsif pos_2_val == pos_3_val && (pos_2_val == "X" || pos_2_val == "O")
-          x[0]
+          "#{x[0]}"
         elsif pos_1_val == pos_3_val && (pos_1_val == "X" || pos_1_val == "O")
-          x[1]
+          "#{x[1]}"
         end
       end
 
