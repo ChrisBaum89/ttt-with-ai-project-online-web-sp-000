3bc7ffcd54652fa40f42709445256f317f20f76e
Chris Baum <christopher.baum@valpo.edu>
2021-02-20 21:08:58 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,6 +7,7 @@ module Players
         pos_1_val = board.cells[x[0]]
         pos_2_val = board.cells[x[1]]
         pos_3_val = board.cells[x[2]]
+        binding.pry
         if pos_1_val == pos_2_val && (pos_1_val == "X" || pos_1_val == "O")
           return x[2]
         elsif pos_2_val == pos_3_val && (pos_2_val == "X" || pos_2_val == "O")
