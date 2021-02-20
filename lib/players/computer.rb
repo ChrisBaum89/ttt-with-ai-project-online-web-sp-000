0b03c2e3c930fa8377a6573a07e61085414a7de4
Chris Baum <christopher.baum@valpo.edu>
2021-02-20 21:03:45 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,9 +6,15 @@ module Players
     def move(board)
       WIN_COMBINATIONS.each do |x|
         pos_1_val = board.cells[x[0]]
-        pos_2_val = board.cells[x[0]]
-        pos_3_val = board.cells[x[0]]
-        binding.pry
+        pos_2_val = board.cells[x[1]]
+        pos_3_val = board.cells[x[2]]
+        if pos_1_val == pos_2_val && (pos_1_val == "X" || pos_1_val == "O")
+          x[2]
+        elsif pos_2_val == pos_3_val && (pos_2_val == "X" || pos_2_val == "O")
+          x[0]
+        elsif pos_1_val == pos_3_val && (pos_1_val == "X" || pos_1_val == "O")
+          x[1]
+        end
       end
 
 
