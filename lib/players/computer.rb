7c95ba499e1a907d7b5385e34c2ab0192a8cf4b4
Chris Baum <christopher.baum@valpo.edu>
2021-02-20 21:16:41 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,17 +8,16 @@ module Players
         pos_2_val = board.cells[x[1]]
         pos_3_val = board.cells[x[2]]
         if pos_1_val == pos_2_val && (pos_1_val == "X" || pos_1_val == "O")
-          chosen = x[2]
+          @chosen = x[2]
         elsif pos_2_val == pos_3_val && (pos_2_val == "X" || pos_2_val == "O")
-          chosen = x[0]
+          @chosen = x[0]
         elsif pos_1_val == pos_3_val && (pos_1_val == "X" || pos_1_val == "O")
-          chosen = x[1]
+          @chosen = x[1]
         else
-          chosen = rand(1..9)
+          @chosen = rand(1..9)
         end
       end
-      chosen
-
+      @chosen
     # def move(board)
     #   rand(1..9)
     # end
